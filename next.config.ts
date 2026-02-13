b2ba17d35eecd367a2fd3f07dc88549d626d1d40
나현종 <nahj0406@naver.com>
2026-02-13 10:04:56 +0900

    config.ts 수정

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone", // node_modules 최소화
   sassOptions: {
     additionalData: `@use "@/css/mixins.scss" as mixins;`,
   },
